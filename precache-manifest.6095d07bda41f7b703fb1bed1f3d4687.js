8541f88de5b666611cc8ee08cb539aa0577071d9
ktrnthsnr <asakthisner@hotmail.com>
2020-07-28 05:29:42 -0700

    Updates

---
+++ precache-manifest.6095d07bda41f7b703fb1bed1f3d4687.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9fa31d071cc1537d44d6f17b7e485ca",
+    "url": "/Apps-Portfolio/index.html"
+  },
+  {
+    "revision": "311783ac17a43e7eff10",
+    "url": "/Apps-Portfolio/static/css/main.f505e1b3.chunk.css"
+  },
+  {
+    "revision": "406a8c0214df9ac8ba05",
+    "url": "/Apps-Portfolio/static/js/2.a40d9510.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Apps-Portfolio/static/js/2.a40d9510.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "311783ac17a43e7eff10",
+    "url": "/Apps-Portfolio/static/js/main.7953bbfc.chunk.js"
+  },
+  {
+    "revision": "bfd1ce02fb6f66606de8",
+    "url": "/Apps-Portfolio/static/js/runtime-main.ce38e94d.js"
+  },
+  {
+    "revision": "feef4d1ecf7db02b77b4066fcf3040e2",
+    "url": "/Apps-Portfolio/static/media/at.feef4d1e.jpg"
+  }
+]);
